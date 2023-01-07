26de7b2857cd8c62566138cead6eb7769698bc05
Saveliy Akoru <74486667+akorutant@users.noreply.github.com>
2023-01-07 12:12:44 +0300

    refactor main file

--- 10 GREAT DISCOVERIES.py
+++ 10 GREAT DISCOVERIES.py
@@ -1,17 +1,14 @@
 import pygame
 import sys
-from os.path import abspath, dirname
 
 from moviepy.editor import VideoFileClip
 
-import sprites.player
 from levels import levels
 from functions.terminate import terminate
-from functions.generate_levels import generate_level
 from functions.load_level import load_level
+from functions.generate_levels import generate_level
 from buttons.menu_button import MenuButton
 from buttons.question_button import QuestionButton
-from sprites.player import Player
 from sprites.all_sprites_groups import spritres_groups
 
 pygame.mixer.pre_init(44100, -16, 1, 512)
@@ -52,11 +49,9 @@ while levels.menu:
     levels.menu = not start_btn.clicked
     pygame.display.update()
 
+player, px, py = generate_level(load_level(f"data/lvl{lvl_number}/level{lvl_number}.txt"), 1)
 # ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 while True:
-    player = generate_level(
-        load_level(f"{dirname(abspath(__file__))}/all_levels/lvl{lvl_number}/level{lvl_number}.txt"), lvl_number,
-        dirname(abspath(__file__)))
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -87,41 +82,41 @@ while True:
         spritres_groups.all_sprites.draw(screen)
         pygame.display.update()
 
-        # ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-        while levels.question:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    terminate()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        flPause = not levels.flPause
-                        if flPause:
-                            pygame.mixer.music.pause()
-                        else:
-                            pygame.mixer.music.unpause()
-            menu_background = pygame.image.load("backgrounds/question1.png")
-            screen.blit(menu_background, (0, 0))
+        # # ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+        # while levels.question:
+        #     for event in pygame.event.get():
+        #         if event.type == pygame.QUIT:
+        #             terminate()
+        #         if event.type == pygame.KEYDOWN:
+        #             if event.key == pygame.K_SPACE:
+        #                 flPause = not levels.flPause
+        #                 if flPause:
+        #                     pygame.mixer.music.pause()
+        #                 else:
+        #                     pygame.mixer.music.unpause()
+        #     menu_background = pygame.image.load("backgrounds/question1.png")
+        #     screen.blit(menu_background, (0, 0))
+        #
+        #     true_btn = QuestionButton(382, 120, (255, 140, 0), (255, 215, 0), screen, True)
+        #     false1_btn = QuestionButton(360, 120, (255, 140, 0), (255, 215, 0), screen, False)
+        #     false2_btn = QuestionButton(350, 120, (255, 140, 0), (255, 215, 0), screen, False)
+        #     true_btn.draw(455, 268, " Колесницы", 90)
+        #     false1_btn.draw(95, 450, "   Бумагу", 90)
+        #     false2_btn.draw(840, 450, "    Порох", 90)
+        #     question = true_btn.answer
+        #     if true_btn.clicked:
+        #         levels.question = False
+        #         lvl_number += 1
+        #         click.play()
+        #     if false1_btn.clicked:
+        #         levels.question = False
+        #         levels.level1 = True
+        #         break
+        #     if false2_btn.clicked:
+        #         levels.question = False
+        #         levels.level1 = True
+        #         break
+        #
+        #     pygame.display.update()
 
-            true_btn = QuestionButton(382, 120, (255, 140, 0), (255, 215, 0), screen, True)
-            false1_btn = QuestionButton(360, 120, (255, 140, 0), (255, 215, 0), screen, False)
-            false2_btn = QuestionButton(350, 120, (255, 140, 0), (255, 215, 0), screen, False)
-            true_btn.draw(455, 268, " Колесницы", 90)
-            false1_btn.draw(95, 450, "   Бумагу", 90)
-            false2_btn.draw(840, 450, "    Порох", 90)
-            question = true_btn.answer
-            if true_btn.clicked:
-                levels.question = False
-                lvl_number += 1
-                click.play()
-            if false1_btn.clicked:
-                levels.question = False
-                levels.level1 = True
-                break
-            if false2_btn.clicked:
-                levels.question = False
-                levels.level1 = True
-                break
-
-            pygame.display.update()
-
-pygame.quit()
+terminate()
