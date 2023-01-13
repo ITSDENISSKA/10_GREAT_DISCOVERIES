515c52f3f1511b549d91c1b3b65e5ebf83ecbc46
Saveliy Akoru <74486667+akorutant@users.noreply.github.com>
2023-01-13 17:48:01 +0300

    add buttons text

--- functions/start_question.py
+++ functions/start_question.py
@@ -2,6 +2,7 @@ import pygame
 
 from buttons.question_button import QuestionButton
 from functions.terminate import terminate
+from buttons_text import text
 from levels import levels
 
 
@@ -23,9 +24,9 @@ def start_question(screen, lvl_number, CLICK_SOUND):
         true_btn = QuestionButton(382, 120, (255, 140, 0), (255, 215, 0), screen, True)
         false1_btn = QuestionButton(360, 120, (255, 140, 0), (255, 215, 0), screen, False)
         false2_btn = QuestionButton(350, 120, (255, 140, 0), (255, 215, 0), screen, False)
-        true_btn.draw(455, 268, " Колесницы", 90)
-        false1_btn.draw(95, 450, "   Бумагу", 90)
-        false2_btn.draw(840, 450, "    Порох", 90)
+        true_btn.draw(455, 268, text[f"level{lvl_number}"][0], 90)
+        false1_btn.draw(95, 450, text[f"level{lvl_number}"][1], 90)
+        false2_btn.draw(840, 450, text[f"level{lvl_number}"][2], 90)
         if true_btn.clicked:
             levels.question = False
             CLICK_SOUND.play()
